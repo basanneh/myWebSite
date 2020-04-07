2b543d1c862c49148aaba8c9adfb4aba8a52d2b2
Baboucarr Sanneh <basanneh@gmail.com>
2020-04-06 23:43:45 -0400

    Added database  'postgres' connection in the settings.py file

--- bsanneh/settings.py
+++ bsanneh/settings.py
@@ -73,14 +73,24 @@ WSGI_APPLICATION = 'bsanneh.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'bsanneh',
+            'USER': 'admin',
+            'PASSWORD': 'P@$$w0rd',
+            'HOST': 'localhost',
+            'PORT': '',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
